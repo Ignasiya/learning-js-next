735c1343b0e824169914940a6d1bd8978dcaf9e6
Ignasiya <ignasiya93@gmail.com>
2024-06-12 10:07:37 +0300

    Павел Тарасов

--- seminar-1/example2.js
+++ seminar-1/example2.js
@@ -1,21 +1,48 @@
+/*
+Задание 2: 
+Создайте обычный объект library. Необходимо реализовать Symbol.iterator, у 
+которого каждая итерация будет возвращать следующую книгу из библиотеки.
+Продемонстрируйте работу Symbol.iterator у нашего объекта.
+*/
+// Список книг:
+
 const books = [
     { title: "1984", author: "George Orwell" },
     { title: "Brave New World", author: "Aldous Huxley" },
     { title: "Fahrenheit 451", author: "Ray Bradbury" },
-]
+];
 
-books[Symbol.iterator] = function () {
-    return {
-        current: 0,
-        to: this.length,
-        next() {
-            return this.current < this.to
-                ? { done: false, value: books[this.current++] }
-                : { done: true };
-        }
-    }
+const library = {
+    books,
+    [Symbol.iterator]() {
+        let index = 0;
+        return {
+            next: () => {
+                if (index < this.books.length) {
+                    return {
+                        value: this.books[index++],
+                        done: false,
+                    };
+                }
+                return {
+                    value: undefined,
+                    done: true,
+                };
+            },
+        };
+    },
+};
+
+
+const iterator = library[Symbol.iterator]();
+console.log(iterator);
+
+for (const book of library) {
+    console.log(book);
 }
 
-for (let book of books) {
-    console.log(`${book.title} - ${book.author}`);
-}
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
--- seminar-1/example4.js
+++ seminar-1/example4.js
@@ -1,13 +1,45 @@
-let lesson = new Map()
-    .set('Математика', 'Смирнов')
-    .set('История', 'Иванова');
+/*
+Задание 4.
+Представьте себе ситуацию: у нас есть группа студентов, и мы хотим отследить, 
+кто из них посетил какие уроки и кто из преподавателей вёл данные уроки. 
+ 
+Необходимо: 
+1. Создать Map объект, который будет использоваться для хранения соответствия 
+между уроком и преподавателем, урок => преподаватель.
+2. Необходимо создать Map объект, ключами которого будут объекты студентов,
+а значениями будут Set объекты, которые будут хранить уроки, посещенные 
+студентом.
+*/
 
-const ivanLessons = new Set()
-    .add('Математика')
-    .add('История');
+const lessonsTeacher = new Map();
+const studentLessons = new Map();
 
-const students = new Map()
-    .set('Иван', ivanLessons);
+const ivan = {
+    name: "Иван",
+};
 
-console.log(`Преподаватель по Метаматике: ${lesson.get('Математика')}`);
-console.log(`Уроки Ивана: ${[...students.get('Иван')]}`);
+const paul = {
+    name: "Паша",
+};
+
+const lessonMath = "Математика";
+const lessonHistory = "История";
+
+const teacherMath = {
+    name: "Dina",
+};
+
+const teacherHistory = {
+    name: "Max",
+};
+
+lessonsTeacher.set(lessonMath, teacherMath).set(lessonHistory, teacherHistory);
+studentLessons.set(ivan, new Set([lessonMath, lessonHistory]));
+studentLessons.set(paul, new Set([lessonHistory]));
+
+// Преподаватель по Математике: Смирнов.
+console.log(
+    `Преподаватель по математике: ${lessonsTeacher.get(lessonMath).name}`
+);
+// Уроки Ивана: Математика, История.
+console.log(`Уроки Ивана: ${[...studentLessons.get(ivan)].join(", ")}`);
